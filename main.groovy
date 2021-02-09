637d3cafbfae0d91f7e2bf72450d0b192e153cc6
jrgomes1 <jesse1819@gmail.com>
2021-02-08 21:52:31 -0500

    working Retry method

--- main.groovy
+++ main.groovy
@@ -1,25 +1,27 @@
 import groovy.json.JsonSlurper
-//import groovy.transform.Field
+import groovy.transform.Field
 import java.nio.charset.StandardCharsets
 import java.util.concurrent.ExecutionException
 
 //def response = sendRequest("GET","https://fatima-jewellery.myshopify.com/admin/api/2021-01/shop.json","",true)
 
 
-def MAX_RETRIES = 5
+@Field def MAX_RETRIES = 5
 def result
 Retry retry = new Retry()
+
+def allProductsCollectionID
+def allProductsHandle
+retry.runWithRetries(MAX_RETRIES, () -> {
+    result = sendRequest("GET", "https://fatima-jewellery.myshopify.com/admin/api/2021-01/smart_collections.json", "", true).result
+    allProductsCollectionID = result.smart_collections[0].id
+    allProductsHandle = result.smart_collections[0].handle
+})
+
+def productCount
+println "$allProductsHandle:$allProductsCollectionID"
+
 while(true) {
-    retry.runWithRetries(MAX_RETRIES, () -> {
-        result = sendRequest("GET", "https://fatima-jewellery.myshopify.com/admin/api/2021-01/smart_collections.json", "", true).result
-    })
-    def allProductsCollectionID = result.smart_collections[0].id
-    def allProductsHandle = result.smart_collections[0].handle
-
-    def productCount
-
-
-    println "$allProductsHandle:$allProductsCollectionID"
     retry.runWithRetries(MAX_RETRIES, () -> {
         productCount = sendRequest("GET", "https://fatima-jewellery.myshopify.com/admin/products/count.json?collection_id=${allProductsCollectionID}", "", true).result.count
     })
@@ -55,12 +57,11 @@ def sendRequest(String reqMethod, String URL, String message, Boolean failOnErro
     def slurper = new JsonSlurper()
     try {
         if(request.getInputStream().available())
-            result = slurper.parseText(request.getInputStream().getText())
-        response.result = result
-    } catch (Exception ignored) {
+            response.result = slurper.parseText(request.getInputStream().getText())
+    } catch (Exception e) {
         if(failOnError){
-            println request.getHeaderFields()
-            assert false : "Request made to $URL failed.\nResponse code is: $getRC\n${request.getResponseMessage()}\n${request.getErrorStream().getText()}"
+            assert false : "Request made to $URL failed.\nResponse code is: $getRC\n${request.getResponseMessage()}\n" +
+                    "${request.getErrorStream().getText()}\nException: $e"
         } else{
             response.result = request.getErrorStream().getText()
         }
@@ -80,9 +81,10 @@ class Retry implements  ThrowingTask {
                 t.run();
                 return true;
             }
-            catch (ExecutionException  e) {
+            catch (Exception  e) {
+                println "RETRYING....."
                 if (++count >= maxRetries)
-                    return false;
+                    throw new Exception("Maximum amount of retries reached, giving up.")
             }
         }
     }
